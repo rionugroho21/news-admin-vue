7ac83ce7a47c96041bcdea18191a89aa538b16a2
Rio Nugroho <rio.nugrosz@gmail.com>
2019-03-21 13:56:42 +0700

    fix

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/')));
 
-app.get('/', function(req,res){
-  res.sendFile('index.html', {root: path.join(__dirname, '/')}) 
-});
+// app.get('/', function(req,res){
+//   res.sendFile('index.html', {root: path.join(__dirname, '/')}) 
+// });
 
-// app.get('/', function (req, res) {
-//   res.render(path.join(__dirname + '/index.html'))
-// })
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
 
 app.listen(8080, function(){
   console.log((new Date()) + " Server is listening on port 8080");
